4622c65fea634de5fe871e487b8c2be79668698d
Rafael Vieira <108036624+VieiraRaffah@users.noreply.github.com>
2023-06-11 15:54:41 -0300

    Calculadora_Imc
    Atividade Matéria POO, Curso ADS UNI

---
+++ CaluladoraImc/src/main/java/com/atividadepoo/caluladoraimc/CaluladoraImc.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.atividadepoo.caluladoraimc;
+
+/**
+ *
+ * @author rafae
+ */
+public class CaluladoraImc {
+
+    public static void main(String[] args) {
+        fmrCalculadora fc = new fmrCalculadora();
+        fc.setVisible(true);
+    }
+}
